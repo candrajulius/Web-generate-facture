03fc792b1c13a143544f6a11b36010d1c01b52e2
candrajulius <candrajulius1@gmail.com>
2025-02-27 16:43:23 +0700

    feat: add create invoice and create invoice item

--- app/Filament/Resources/ServiceResource.php
+++ app/Filament/Resources/ServiceResource.php
@@ -4,12 +4,14 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\ServiceResource\Pages;
 use App\Filament\Resources\ServiceResource\RelationManagers;
+use App\Models\Customer;
 use App\Models\Service;
 use Filament\Forms;
 use Filament\Forms\Components\Repeater;
 use Filament\Forms\Components\Section;
 use Filament\Forms\Components\Split;
 use Filament\Forms\Form;
+use Filament\Forms\FormsComponent;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
@@ -41,6 +43,7 @@ class ServiceResource extends Resource
                     ->description('Fitur - Fitur yang dibuat untuk layanan anda')
                     ->schema([
                         Repeater::make('features')
+                            ->relationship('features')
                             ->schema([
                                 Forms\Components\TextInput::make('name')
                                     ->label('Nama Fitur')
@@ -65,19 +68,26 @@ class ServiceResource extends Resource
             ->columns([
                 Tables\Columns\TextColumn::make('name')
                     ->searchable(),
-                Tables\Columns\TextColumn::make('created_at')
-                    ->dateTime()
-                    ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
-                Tables\Columns\TextColumn::make('updated_at')
-                    ->dateTime()
-                    ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('features.name')
+                    ->label('Fitur yang dibuat'),
             ])
             ->filters([
                 //
             ])
             ->actions([
+                Tables\Actions\Action::make('add_invoice')
+                    ->label('Buat Invoice')
+                    ->form([
+                        Forms\Components\Select::make('customer_id')
+                            ->label('Pelanggan')
+                            ->options(Customer::pluck('name','id'))
+                            ->searchable()
+                            ->required(),
+                    ])
+                    ->requiresConfirmation()
+                    ->action(function (Service $service, array $data){
+                        $service->createInvoice($data['customer_id']);
+                    }),
                 Tables\Actions\EditAction::make(),
             ])
             ->bulkActions([
--- app/Models/Service.php
+++ app/Models/Service.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Filament\Facades\Filament;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -14,5 +15,26 @@ class Service extends Model
     public function features()
     {
         return $this->hasMany(Feature::class,'feature_id');
+    }
+
+    public function createInvoice($user_id)
+    {
+        $invoice = Invoice::create([
+            'customer_id' => $user_id,
+            'service_id' => $this->id,
+            'invoice_number' => 'INV-' . time(),
+            'total_price' => collect($this->features)->sum('price'),
+            'status' => 'pending',
+        ]);
+
+        foreach($this->features as $feature)
+        {
+            InvoiceItem::create([
+                'invoice_id' => $invoice->id,
+                'name' => $feature['name'],
+                'description' => $feature['description'],
+                'price' => $feature['price'],
+            ]);
+        }
     }
 }
